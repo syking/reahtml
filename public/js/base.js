3344c13124fea744211d9c5381a147f83ad69a9c
ysc8620 <ysc8620@163.com>
2014-06-06 11:50:39 +0800

    base code

---
+++ public/js/base.js
@@ -0,0 +1,34 @@
+$(function(){setLeftNavTop();
+	$('.item_block').click(function(){
+		$('.dialog_bg').show();
+    	$('.item_dialog_block').show();
+	});
+	$('.dialog .close_dialog').click(function(){
+		// 重置表单
+		$(this).parent().parent().find('form')[0].reset();
+		// 关闭弹窗
+		$(this).parent().parent().hide();
+		$('.dialog_bg').hide();
+	});
+	$('.cutover_block').click(function(){
+		$('.'+$(this).attr('data-select')).removeClass('current');
+		$(this).addClass('current');
+		$('.'+$(this).attr('data-id')).addClass('current');
+	});
+	
+	$('.jsUrl').click(function(){
+		window.location.href = $(this).attr('data-url');
+	});
+});
+$(document).resize(function(){setLeftNavTop();});
+$(document).scroll(function(){setLeftNavTop();});
+function setLeftNavTop()
+{
+	var top = $(document).scrollTop();
+	if(top>155)
+	{
+		$('.left_nav').addClass('current');
+	}else{
+		$('.left_nav').removeClass('current');
+	}
+}
